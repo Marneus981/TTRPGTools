d95d202e3b23b65093a3eb1dfed28d5e2467759f
Marneus981 <md.sora@hotmail.com>
2023-07-31 14:29:01 -0600

    First Sketch of Settings Class

--- src/master.h
+++ src/master.h
@@ -1,7 +1,7 @@
 #pragma once
 //STATS_CLASS.H/.CPP
 #include <vector>
-#include <string>
+#include <string>-
 #include <iostream>
 #include <fstream>
 #include <sstream>
@@ -11,6 +11,28 @@
 
 namespace std{
     vector<pair<string, vector<string>>> get_settings(string setting, string path_to_game);
+
+    class setting{
+        public:
+            setting();
+            ~setting();
+            int refresh_sub_settings();
+        protected:
+        private:
+            map<string, vector<string>> _sub_settings;
+            map<string,int> _setting_types;
+
+    };
+    class game_config {
+        public:
+            game_config(string path_to_game);
+            ~game_config();
+            int refresh_settings();
+        protected:
+        private:
+            string _path_to_game;
+            map<string, vector<setting>> _settings;
+    };
 }
 
 
