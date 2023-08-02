f34deb4b7f256118504ba9559722bd06e00c5afc
Marneus981 <md.sora@hotmail.com>
2023-08-01 20:38:36 -0600

    Now for realsies

--- src/master.h
+++ src/master.h
@@ -12,28 +12,37 @@
 namespace std{
     //Below is a legacy function, probably getting rid of it
     vector<pair<string, vector<string>>> get_settings(string setting, string path_to_game);
-
+    
     class setting{
+        friend setting parse_settings(string path_to_game);
         public:
             setting(string settings, vector<string> sub_settings, vector<int> sub_setting_types);
             ~setting();
             int refresh_sub_settings();
+            //Set function?
+            //Get function?
         protected:
         private:
             map<string, vector<string>> _Sub_Settings;
             map<string,int> _Types;
+            //Types: {0: int, 1: float, 2: string, 3: bool}
 
     };
     class game_config {
+        friend setting parse_settings(string path_to_game);
         public:
             game_config(string path_to_game);
             ~game_config();
             int refresh_settings();
+            //Set function?
+            //Get function?
         protected:
         private:
             string _Path_To_Game;
             map<string, vector<setting>> _Settings;
     };
+    //Setting parser
+    setting parse_settings(string path_to_game);
 }
 
 
